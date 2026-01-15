c598b6242f6296571968a814da8c01ea17f733b3
Jake Correnti <jakecorrenti+github@proton.me>
2026-01-15 13:48:14 -0500

    curl imds for a token
    Signed-off-by: Jake Correnti <jakecorrenti+github@proton.me>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io::{Write, stdout};
+
+use curl::easy::{Easy, List};
+
 fn main() {
-    println!("Hello, world!");
+    let mut list = List::new();
+    list.append("X-aws-ec2-metadata-token-ttl-seconds: 10")
+        .unwrap();
+
+    let mut easy = Easy::new();
+    easy.url("http://169.254.169.254/latest/api/token").unwrap();
+    easy.http_headers(list).unwrap();
+    easy.put(true).unwrap();
+
+    easy.write_function(|data| {
+        stdout().write_all(data).unwrap();
+        Ok(data.len())
+    })
+    .unwrap();
+    easy.perform().unwrap();
 }
