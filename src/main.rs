52277508212686ff8929caed74ed5d86b4b16f58
Jake Correnti <jakecorrenti+github@proton.me>
2026-01-15 14:28:59 -0500

    query top level metadata
    Signed-off-by: Jake Correnti <jakecorrenti+github@proton.me>

--- src/main.rs
+++ src/main.rs
@@ -25,4 +25,26 @@ fn main() {
 
     let token = String::from_utf8(put_response).unwrap();
     println!("put_response: {:#?}", token);
+
+    easy.url("http://169.254.169.254/latest/meta-data").unwrap();
+    list = List::new();
+    list.append(&format!("X-aws-ec2-metadata-token: {}", token))
+        .unwrap();
+    easy.http_headers(list).unwrap();
+    easy.put(false).unwrap();
+
+    let mut toplevel_metadata_response = Vec::new();
+    {
+        let mut transfer = easy.transfer();
+        transfer
+            .write_function(|buf| {
+                toplevel_metadata_response.extend_from_slice(buf);
+                Ok(buf.len())
+            })
+            .unwrap();
+        transfer.perform().unwrap();
+    }
+
+    println!();
+    println!("{}", String::from_utf8(toplevel_metadata_response).unwrap());
 }
